499582851c43f5c241d00b77e99b1dd7369e583c
Adrià Fontcuberta <afontcu@gmail.com>
2019-03-16 21:00:09 +0100

    Give it some prettiness (is that even a word)

--- theme.js
+++ theme.js
@@ -1,53 +1,67 @@
-import { future as theme } from 'mdx-deck/themes'
+import { code as theme } from 'mdx-deck/themes'
 import style from 'react-syntax-highlighter/styles/prism/ghcolors'
-import scss from 'react-syntax-highlighter/languages/prism/scss'
+import bash from 'react-syntax-highlighter/languages/prism/bash'
 
-const bodyFont = 'Martel, serif'
-const titleFont = 'Kavoon, serif'
+const bodyFont = 'Merriweather, serif'
+const titleFont = '"Kavoon", serif'
 const mainColor = '#f06c17'
 
 export default {
   ...theme,
-  monospace: bodyFont,
-  font: titleFont,
+  // monospace: bodyFont,
+  font: bodyFont,
   prism: {
     style,
     languages: {
-      scss,
+      bash,
     },
   },
+  colors: {
+    text: 'white',
+  },
   css: {
-    ...theme.css,
-    textAlign: 'left',
-    blockquote: {
-      fontSize: '4rem',
-      fontFamily: titleFont,
-    },
-    li: {
-      fontFamily: bodyFont,
-      fontSize: '1.4em',
-      paddingBottom: '1.2em',
+    backgroundImage: 'linear-gradient(90deg,#e6007d 0,#f8a022)',
+    fontSize: '2rem',
+    'table td': {
+      padding: '15px',
     },
     pre: {
       borderRadius: '8px',
       boxShadow: '2px 4px 10px rgba(0, 0, 0, 0.3)',
     },
-    strong: {
-      fontWeight: 900,
-      color: mainColor,
-    } 
   },
-  heading: {
-    textTransform: 'inherit',
-  },
-  colors: {
-    text: '#333',
-    background: '#fafafa',
-  },
-  p: {
-    fontFamily: bodyFont,
-    fontSize: '1.5em',
-    lineHeight: '1.4',
-  },
+  //   textAlign: 'left',
+  //   blockquote: {
+  //     fontSize: '4rem',
+  //     fontFamily: titleFont,
+  //   },
+  //   li: {
+  //     fontFamily: bodyFont,
+  //     fontSize: '1.4em',
+  //     paddingBottom: '1.2em',
+  //   },
+  //   strong: {
+  //     fontWeight: 900,
+  //     color: mainColor,
+  //   },
+  //   h1: {
+  //     fontFamily: titleFont,
+  //   },
+  //   h2: {
+  //     fontFamily: titleFont,
+  //   },
+  // },
+  // heading: {
+  //   textTransform: 'inherit',
+  // },
+  // colors: {
+  //   text: '#333',
+  //   background: '#fafafa',
+  // },
+  // p: {
+  //   fontFamily: bodyFont,
+  //   fontSize: '1.5em',
+  //   lineHeight: '1.4',
+  // },
   transitionDuration: 0, // disable transitions
 }
