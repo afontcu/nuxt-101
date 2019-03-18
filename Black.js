7960ab1c78b270d5452b0c622ee0fcc8f42a6db2
Adrià Fontcuberta <afontcu@gmail.com>
2019-03-18 23:29:13 +0100

    Add stupid meme

---
+++ Black.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default ({ children }) => <div
+  style={{
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+  }}>{children}</div>
--- theme.js
+++ theme.js
@@ -15,6 +15,7 @@ export default {
   },
   colors: {
     text: 'white',
+    link: 'rgba(255, 255, 255, 0.9)',
   },
   css: {
     backgroundImage: 'linear-gradient(90deg, #e6007d 0, #f8a022)',
@@ -27,7 +28,14 @@ export default {
       boxShadow: '2px 4px 10px rgba(0, 0, 0, 0.3)',
       textShadow: 'none',    
     },
-    textShadow: '4px 3px 8px rgba(0, 0, 0, 0.2)',    
+    textShadow: '4px 3px 8px rgba(0, 0, 0, 0.2)',  
+    a: {
+      textAlign: 'center',
+      display: 'block',
+      '&:hover': {
+        color: 'white'
+      }
+    },
   },
   transitionDuration: 0, // disable transitions
 }
